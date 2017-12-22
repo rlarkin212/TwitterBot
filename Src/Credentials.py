26411fb8f999835a797cc674275ea2402ad452a6
40107426 <rlarkin03@qub.ac.uk>
2017-12-22 23:58:43 +0000

    deleted file

--- Src/Credentials.py
+++
@@ -1,5 +0,0 @@
-consumer_key = 'Qfc0YiY7cctBPvwM50O6PFm2d'
-consumer_secret = 'PnXkvpvJ3LVi1dTFixGvuzlrAby4mckrmDX11OiE5REuZ7AMr1'
-access_token = '944207446783987712-8kG7gTPn8fp9isjuHBfhz8eYUkHq8gm'
-access_token_secret = '3hPYK6ldBWV2kYNthjvWtxqqCa1IHodw1hBAoQSIYqro5'
-
