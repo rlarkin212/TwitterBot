1eda9ac8e3279fe9c4c0359c2bd59985b9867d26
40107426 <rlarkin03@qub.ac.uk>
2017-12-22 15:00:50 +0000

    Tweets every 15 mins, retweetes every 14

---
+++ Src/twitterbot_retweet.py
@@ -0,0 +1,31 @@
+import tweepy
+from time import sleep
+from credentials import *
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+for tweet in tweepy.Cursor(api.search, q='#lilPump').items():
+    try:
+        print('\nTweet by: @' + tweet.user.screen_name)
+
+        tweet.retweet()
+        print('Retweeted the tweet')
+
+        # Favorite the tweet
+        tweet.favorite()
+        print('Favorited the tweet')
+
+        # Follow the user who tweeted
+        if not tweet.user.following:
+            tweet.user.follow()
+            print('Followed the user')
+
+        sleep(800)
+
+    except tweepy.TweepError as e:
+        print(e.reason)
+
+    except StopIteration:
+        break
--- Src/twitterbot_textfile.py
+++ Src/twitterbot_textfile.py
@@ -1,27 +1,29 @@
-# Import our Twitter credentials from credentials.py
-from Credentials import *
+from credentials import *
 import tweepy
 from time import sleep
 
-# Access and authorize our Twitter credentials from credentials.py
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
-myfile = open('lilpump.txt','r')
-fileLines = myfile.readlines()
-myfile.close()
+my_file = open('lilpump.txt','r')
+file_lines = my_file.readlines()
+my_file.close()
 
-#loop to go over lines
-for line in fileLines:
 
-    try:
-        print(line)
-        if line != '\n':
-            api.update_status(line)
-        else:
-            pass
-    except tweepy.TweepError as e:
-        print(e.reason)
-    
-    sleep(5)
+# Tweet a line every 15 minutes
+def tweet():
+    for line in file_lines:
+        try:
+             print(line)
+             if line != '\n':
+                 api.update_status(line + '#esketit #lilpump')
+                 sleep(900)
+             else:
+                pass
+        except tweepy.TweepError as e:
+            print(e.reason)
+            sleep(2)
+
+tweet()
+
